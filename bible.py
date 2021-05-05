b9e59197a126f13fec3103f24e278ef2e6e1b6fe
lopsaman <72528639+lopsaman@users.noreply.github.com>
2021-05-05 16:59:00 -0400

    Add files via upload

---
+++ bible.py
@@ -0,0 +1,11 @@
+#bible guide
+
+import guibible.net
+import pygame
+
+sample1 = guibible.net.Bible("John", 3, 16)
+sample2 = guibible.net.Bible("Ephesians",3,16)
+
+
+sample1.read()
+sample2.read()
